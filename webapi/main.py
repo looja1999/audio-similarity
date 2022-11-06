ff618d561469a48fa7ad169e21979981b594ebff
Looja <67297850+looja1999@users.noreply.github.com>
2022-11-06 14:46:55 +0930

    Audio saved in fastapi

--- webapi/main.py
+++ webapi/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
+import wave
+import shutil
 
 app = FastAPI() 
 
 @app.get("/")
 def default():
   return {"status" : "working"} 
+
+@app.get("/about")
+def about():
+  return {
+    "about" : "This api helps to compare two voices passed from frontend"
+  }
+
+@app.post("/upload")
+async def upload(file : UploadFile = File(...)):
+  try:
+    with open("./saved_audio/{}".format(file.filename), "wb") as buffer:
+      shutil.copyfileobj(file.file, buffer)
+  finally:
+        file.file.close()
+  return file.filename
